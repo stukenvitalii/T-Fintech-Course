0e685d340bcd23bd171bbed05e70af2d50137c74
stukenvitalii <stukenvitalii@gmail.com>
2024-09-05 14:14:48 +0300

    change default logger to sl4j logger, make XmlMapper field final static

--- src/main/java/edu/tbank/parser/XmlParser.java
+++ src/main/java/edu/tbank/parser/XmlParser.java
@@ -2,23 +2,22 @@ package edu.tbank.parser;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import edu.tbank.dto.City;
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
+@Slf4j
+@UtilityClass
 public class XmlParser {
-    private XmlParser() {}
-    private static final Logger logger = Logger.getLogger(XmlParser.class.getName());
-
+    private final static XmlMapper xmlMapper = new XmlMapper();
     public static void convertStringToXMLFile(City city) {
-        XmlMapper xmlMapper = new XmlMapper();
         try {
             xmlMapper.writeValue(new File("response.xml"), city);
-            logger.log(Level.INFO, "XML file written to response.xml");
+            log.info("XML file written to response.xml");
         } catch (IOException e) {
-            logger.log(Level.SEVERE, e.getMessage(), e);
+            log.error(e.getMessage(), e);
         }
     }
 }
