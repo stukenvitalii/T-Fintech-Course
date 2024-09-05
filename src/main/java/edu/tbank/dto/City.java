2e8c0063cc18ba14806dab51dcb115e2f6850778
stukenvitalii <stukenvitalii@gmail.com>
2024-09-05 14:15:10 +0300

    make fields final

--- src/main/java/edu/tbank/dto/City.java
+++ src/main/java/edu/tbank/dto/City.java
@@ -2,22 +2,19 @@ package edu.tbank.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class City implements Serializable {
-    private String slug;
-    private Coordinates coords;
+    private final String slug;
+    private final Coordinates coords;
 
     @Data
-    @NoArgsConstructor
     @AllArgsConstructor
     public static class Coordinates {
-        private double lat;
-        private double lon;
+        private final double lat;
+        private final double lon;
     }
 }
