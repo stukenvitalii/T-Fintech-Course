26f333962695bfff999fdbf1c06aa2161babccda
stukenvitalii <stukenvitalii@gmail.com>
2024-09-06 23:18:36 +0300

    add CustomLinkedList interface

---
+++ src/main/java/edu/tbank/hw3/CustomLinkedList.java
@@ -0,0 +1,16 @@
+package edu.tbank.hw3;
+
+import java.util.List;
+
+public interface CustomLinkedList<T> {
+    void add(T t);
+    T get(int index);
+    void remove(int index);
+    int size();
+    boolean contains(T t);
+    void addAll(List<T> list);
+
+    private boolean isEmpty() {
+        return false;
+    }
+}
