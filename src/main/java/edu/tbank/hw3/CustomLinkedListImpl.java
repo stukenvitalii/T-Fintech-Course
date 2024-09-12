0a5eac0596ed7a51c8f73b410594aefaa300606c
stukenvitalii <stukenvitalii@gmail.com>
2024-09-12 22:54:00 +0300

    implement a checkIndex() method instead of checking manually

--- src/main/java/edu/tbank/hw3/CustomLinkedListImpl.java
+++ src/main/java/edu/tbank/hw3/CustomLinkedListImpl.java
@@ -22,19 +22,17 @@ public class CustomLinkedListImpl<T> implements CustomLinkedList<T> {
         return head == null;
     }
 
+    private void checkIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is out of bounds");
+        }
+    }
+
     @Override
     public T get(int index) {
-        if (index < 0) {
-            throw new IndexOutOfBoundsException("Index cannot be negative");
-        }
-        if (isEmpty()) {
-            throw new IndexOutOfBoundsException("List is empty");
-        }
+        checkIndex(index);
         Node<T> current = head;
         for (int i = 0; i < index; i++) {
-            if (current == null) {
-                throw new IndexOutOfBoundsException("Index is out of bounds");
-            }
             current = current.getNext();
         }
         return current.getData();
@@ -54,12 +52,7 @@ public class CustomLinkedListImpl<T> implements CustomLinkedList<T> {
 
     @Override
     public void remove(int index) {
-        if (index < 0) {
-            throw new IndexOutOfBoundsException("Index cannot be negative");
-        }
-        if (isEmpty()) {
-            throw new IndexOutOfBoundsException("List is empty");
-        }
+        checkIndex(index);
         if (index == 0) {
             head = head.getNext();
             size--;
@@ -67,13 +60,7 @@ public class CustomLinkedListImpl<T> implements CustomLinkedList<T> {
         }
         Node<T> current = head;
         for (int i = 0; i < index - 1; i++) {
-            if (current == null) {
-                throw new IndexOutOfBoundsException("Index is out of bounds");
-            }
             current = current.getNext();
-        }
-        if (current.getNext() == null) {
-            throw new IndexOutOfBoundsException("Index is out of bounds");
         }
         current.setNext(current.getNext().getNext());
         size--;
