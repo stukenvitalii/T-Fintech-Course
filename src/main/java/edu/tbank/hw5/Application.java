40cc07a1bab68d8da37d08e2e27f1c19a1b9969c
stukenvitalii <stukenvitalii@gmail.com>
2024-09-24 01:00:28 +0300

    add scanBasePackages parameter

--- src/main/java/edu/tbank/hw5/Application.java
+++ src/main/java/edu/tbank/hw5/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"edu.tbank.hw5", "org.tinkoff.measurementloggingstarter"})
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class Application {
     public static void main(String[] args) {
