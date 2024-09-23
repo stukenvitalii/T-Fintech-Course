ce888004c9ab744641f18a078d1f9e7bf3f2a764
stukenvitalii <stukenvitalii@gmail.com>
2024-09-23 22:30:07 +0300

    add implementation to service layer

---
+++ src/main/java/edu/tbank/hw5/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,40 @@
+package edu.tbank.hw5.service.impl;
+
+import edu.tbank.hw5.dto.Category;
+import edu.tbank.hw5.repository.CategoryRepository;
+import edu.tbank.hw5.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryServiceImpl implements CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    @Override
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    @Override
+    public void addCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    @Override
+    public void updateCategoryById(Category category, Long id) {
+        categoryRepository.update(category, id);
+    }
+
+    @Override
+    public void deleteCategoryById(Long id) {
+        categoryRepository.deleteById(id);
+    }
+}
