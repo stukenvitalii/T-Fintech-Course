19ce47cfe212143323e49b28e4fc09424539df96
stukenvitalii <stukenvitalii@gmail.com>
2024-09-23 22:27:59 +0300

    add service layer with all methods needed

---
+++ src/main/java/edu/tbank/hw5/service/CategoryService.java
@@ -0,0 +1,15 @@
+package edu.tbank.hw5.service;
+
+import edu.tbank.hw5.dto.Category;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface CategoryService {
+    List<Category> findAll();
+    Category getCategoryById(Long id);
+    void addCategory(Category category);
+    void updateCategoryById(Category category, Long id);
+    void deleteCategoryById(Long id);
+}
