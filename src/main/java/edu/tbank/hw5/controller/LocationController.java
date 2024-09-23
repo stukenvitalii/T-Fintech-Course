e7697dff0bcf79fd5ced08950f135a84839d01b4
stukenvitalii <stukenvitalii@gmail.com>
2024-09-23 22:30:37 +0300

    add LocationController

---
+++ src/main/java/edu/tbank/hw5/controller/LocationController.java
@@ -0,0 +1,40 @@
+package edu.tbank.hw5.controller;
+
+import edu.tbank.hw5.dto.Location;
+import edu.tbank.hw5.service.LocationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/locations")
+public class LocationController {
+    private final LocationService locationService;
+
+    @GetMapping
+    public List<Location> getAllLocations() {
+        return locationService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Location getLocationById(@PathVariable String id) {
+        return locationService.getLocationById(id);
+    }
+
+    @PostMapping("/add")
+    public void addLocation(@RequestBody Location location) {
+        locationService.addLocation(location);
+    }
+
+    @PutMapping("/update/{id}")
+    public void updateLocationById(@RequestBody Location location, @PathVariable String id) {
+        locationService.updateLocationById(location, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteLocationById(@PathVariable String id) {
+        locationService.deleteLocationById(id);
+    }
+}
