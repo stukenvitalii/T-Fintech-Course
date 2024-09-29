46a6ccdac4abca7f5a6c8aca8fa13c40f51aa5be
stukenvitalii <stukenvitalii@gmail.com>
2024-09-29 22:51:27 +0300

    rename annotation

--- measurement-logging-starter/src/main/java/org/tinkoff/measurementloggingstarter/annotation/TimedAspect.java
+++ measurement-logging-starter/src/main/java/org/tinkoff/measurementloggingstarter/annotation/TimedAspect.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Aspect
 @Component
-public class EnableTimeMeasurementLoggingAspect {
+public class TimedAspect {
 
-    @Around("execution(* *(..)) && (@within(org.tinkoff.measurementloggingstarter.annotation.EnableTimeMeasurementLogging) || @annotation(org.tinkoff.measurementloggingstarter.annotation.EnableTimeMeasurementLogging))")
+    @Around("execution(* *(..)) && (@within(org.tinkoff.measurementloggingstarter.annotation.Timed) || @annotation(org.tinkoff.measurementloggingstarter.annotation.Timed))")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         long initTime = System.currentTimeMillis();
         Object proceed = joinPoint.proceed();
@@ -22,10 +22,8 @@ public class EnableTimeMeasurementLoggingAspect {
         String methodName = signature.getMethod().getName();
         String className = signature.getDeclaringTypeName();
 
-        log.info("============================================================================================================");
         log.info("Class: {}, Method: {}", className, methodName);
         log.info("Method executed in: {} ms", executionTime);
-        log.info("============================================================================================================");
         return proceed;
     }
 }
--- measurement-logging-starter/src/main/java/org/tinkoff/measurementloggingstarter/configuration/AspectAutoConfiguration.java
+++ measurement-logging-starter/src/main/java/org/tinkoff/measurementloggingstarter/configuration/AspectAutoConfiguration.java
@@ -3,14 +3,14 @@ package org.tinkoff.measurementloggingstarter.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.tinkoff.measurementloggingstarter.annotation.EnableTimeMeasurementLoggingAspect;
+import org.tinkoff.measurementloggingstarter.annotation.TimedAspect;
 
 @Configuration
 @EnableAspectJAutoProxy
 public class AspectAutoConfiguration {
 
     @Bean
-    public EnableTimeMeasurementLoggingAspect enableTimeMeasurementLoggingAspect() {
-        return new EnableTimeMeasurementLoggingAspect();
+    public TimedAspect enableTimeMeasurementLoggingAspect() {
+        return new TimedAspect();
     }
 }
