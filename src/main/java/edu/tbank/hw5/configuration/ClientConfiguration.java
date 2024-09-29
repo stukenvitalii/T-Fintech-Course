b62b1e7052da3ad24f3b7e0f2ad3122a8f10c97c
stukenvitalii <stukenvitalii@gmail.com>
2024-09-29 23:21:04 +0300

    use rest client - doesn't work

--- src/main/java/edu/tbank/hw5/configuration/ClientConfiguration.java
+++ src/main/java/edu/tbank/hw5/configuration/ClientConfiguration.java
@@ -3,19 +3,23 @@ package edu.tbank.hw5.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.client.RestClient;
 
 @Configuration
 public class ClientConfiguration {
     @Value("${app.kuda-go-url}")
     private String kudaGoBaseUrl;
 
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(kudaGoBaseUrl));
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        RestTemplate restTemplate = new RestTemplate();
+//        restTemplate.setUriTemplateHandler(new DefaultUriBuilderFactory(kudaGoBaseUrl));
+//
+//        return restTemplate;
+//    }
 
-        return restTemplate;
+    @Bean
+    public RestClient restClient() {
+        return RestClient.builder().baseUrl(kudaGoBaseUrl).build();
     }
 }
