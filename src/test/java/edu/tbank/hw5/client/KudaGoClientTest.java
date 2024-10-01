64dd886650434f28d72df6ff7bf55fb775f5e5a4
stukenvitalii <stukenvitalii@gmail.com>
2024-10-02 00:54:28 +0300

    rewrite test for client using Test Containers

--- src/test/java/edu/tbank/hw5/client/KudaGoClientTest.java
+++ src/test/java/edu/tbank/hw5/client/KudaGoClientTest.java
@@ -1,36 +1,31 @@
 package edu.tbank.hw5.client;
 
-import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import edu.tbank.hw5.dto.Category;
 import edu.tbank.hw5.dto.Location;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.wiremock.integrations.testcontainers.WireMockContainer;
 
-import java.util.Collections;
 import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@ExtendWith(MockitoExtension.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Testcontainers
 @TestPropertySource(locations = "classpath:application-test.properties")
 public class KudaGoClientTest {
 
-    private WireMockServer wireMockServer;
+    @Container
+    static WireMockContainer wiremockServer = new WireMockContainer("wiremock/wiremock:3.6.0");
 
     @Autowired
     private KudaGoClient kudaGoClient;
@@ -43,42 +38,32 @@ public class KudaGoClientTest {
 
     @DynamicPropertySource
     public static void setProperties(DynamicPropertyRegistry registry) {
-        registry.add("app.kuda-go-url", ()->"http://localhost:7070");
-    }
-
-    @BeforeEach
-    public void setup() {
-        wireMockServer = new WireMockServer(new WireMockConfiguration().port(7070));
-        wireMockServer.start();
-        WireMock.configureFor("localhost", 7070);
-    }
-
-    @AfterEach
-    public void tearDown() {
-        wireMockServer.stop();
+        registry.add("app.kuda-go-url", wiremockServer::getBaseUrl);
     }
 
     @Test
-    public void testGetAllCategories() {
+    void testGetAllCategories() {
         // Given
+        WireMock.configureFor(wiremockServer.getHost(), wiremockServer.getFirstMappedPort());
         String categoriesJson = "[{\"id\":1,\"name\":\"Category 1\"},{\"id\":2,\"name\":\"Category 2\"}]";
-        wireMockServer.stubFor(get(urlEqualTo(categoriesEndpoint))
+        stubFor(get(urlEqualTo(categoriesEndpoint))
                 .willReturn(aResponse()
                         .withStatus(200)
-                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withHeader("Content-Type", "application/json")
                         .withBody(categoriesJson)));
 
         // When
         List<Category> categories = kudaGoClient.getAllCategories();
 
         // Then
-        assertEquals(2, categories.size());
+        assertThat(categories).hasSize(2);
     }
 
     @Test
-    public void testGetAllCategoriesError() {
+    void testGetAllCategoriesError() {
         // Given
-        wireMockServer.stubFor(get(urlEqualTo(categoriesEndpoint))
+        WireMock.configureFor(wiremockServer.getHost(), wiremockServer.getFirstMappedPort());
+        stubFor(get(urlEqualTo(categoriesEndpoint))
                 .willReturn(aResponse()
                         .withStatus(500)));
 
@@ -86,30 +71,32 @@ public class KudaGoClientTest {
         List<Category> categories = kudaGoClient.getAllCategories();
 
         // Then
-        assertEquals(Collections.emptyList(), categories);
+        assertThat(categories).isEmpty();
     }
 
     @Test
-    public void testGetAllLocations() {
+    void testGetAllLocations() {
         // Given
-        String locationsJson = "[{\"id\":1,\"name\":\"Location 1\"},{\"id\":2,\"name\":\"Location 2\"}]";
-        wireMockServer.stubFor(get(urlEqualTo(locationsEndpoint))
+        WireMock.configureFor(wiremockServer.getHost(), wiremockServer.getFirstMappedPort());
+        String locationsJson = "[{\"slug\":\"slug1\",\"name\":\"Location 1\"},{\"slug\":\"slug2\",\"name\":\"Location 2\"}]";
+        stubFor(get(urlEqualTo(locationsEndpoint))
                 .willReturn(aResponse()
                         .withStatus(200)
-                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withHeader("Content-Type", "application/json")
                         .withBody(locationsJson)));
 
         // When
         List<Location> locations = kudaGoClient.getAllLocations();
 
         // Then
-        assertEquals(2, locations.size());
+        assertThat(locations).hasSize(2);
     }
 
     @Test
-    public void testGetAllLocationsError() {
+    void testGetAllLocationsError() {
         // Given
-        wireMockServer.stubFor(get(urlEqualTo(locationsEndpoint))
+        WireMock.configureFor(wiremockServer.getHost(), wiremockServer.getFirstMappedPort());
+        stubFor(get(urlEqualTo(locationsEndpoint))
                 .willReturn(aResponse()
                         .withStatus(500)));
 
@@ -117,6 +104,6 @@ public class KudaGoClientTest {
         List<Location> locations = kudaGoClient.getAllLocations();
 
         // Then
-        assertEquals(Collections.emptyList(), locations);
+        assertThat(locations).isEmpty();
     }
 }
