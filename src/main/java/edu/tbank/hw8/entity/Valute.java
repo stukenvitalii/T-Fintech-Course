0590200220be612a5d3ce7b6c3dffc039b2b1f32
stukenvitalii <stukenvitalii@gmail.com>
2024-10-04 23:27:34 +0300

    add entities

---
+++ src/main/java/edu/tbank/hw8/entity/Valute.java
@@ -0,0 +1,29 @@
+// Valute.java
+package edu.tbank.hw8.entity;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import lombok.Data;
+
+@Data
+public class Valute {
+    @JacksonXmlProperty(isAttribute = true, localName = "ID")
+    private String id;
+
+    @JacksonXmlProperty(localName = "NumCode")
+    private String numCode;
+
+    @JacksonXmlProperty(localName = "CharCode")
+    private String charCode;
+
+    @JacksonXmlProperty(localName = "Nominal")
+    private int nominal;
+
+    @JacksonXmlProperty(localName = "Name")
+    private String name;
+
+    @JacksonXmlProperty(localName = "Value")
+    private String value;
+
+    @JacksonXmlProperty(localName = "VunitRate")
+    private String vunitRate;
+}
