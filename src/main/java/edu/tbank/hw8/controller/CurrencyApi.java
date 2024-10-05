8e43bcceaa93a5cb1a78b211186d8443244b05fb
stukenvitalii <stukenvitalii@gmail.com>
2024-10-05 12:55:56 +0300

    add api interface for swagger annotations

---
+++ src/main/java/edu/tbank/hw8/controller/CurrencyApi.java
@@ -0,0 +1,37 @@
+package edu.tbank.hw8.controller;
+
+import edu.tbank.hw8.dto.ConvertRequest;
+import edu.tbank.hw8.dto.ConvertResponse;
+import edu.tbank.hw8.dto.ValuteDto;
+import edu.tbank.hw8.exception.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface CurrencyApi {
+
+    @Operation(summary = "Get currency rate")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ValuteDto.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "503", description = "Service Unavailable", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @GetMapping("/rates/{code}")
+    ValuteDto getCurrencyRate(@PathVariable String code);
+
+    @Operation(summary = "Convert currency")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ConvertResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Bad Request", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", description = "Not Found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "503", description = "Service Unavailable", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class)))
+    })
+    @GetMapping("/convert")
+    ConvertResponse convertCurrency(@RequestBody ConvertRequest request);
+}
