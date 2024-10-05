737e104ad0a2d8b401b94468fca91c59688e502b
stukenvitalii <stukenvitalii@gmail.com>
2024-10-05 12:54:30 +0300

    configure circuit braker in BankClient

--- src/main/java/edu/tbank/hw8/client/BankClient.java
+++ src/main/java/edu/tbank/hw8/client/BankClient.java
@@ -2,18 +2,23 @@ package edu.tbank.hw8.client;
 
 import edu.tbank.hw8.entity.ValCurs;
 import edu.tbank.hw8.entity.Valute;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class BankClient {
     private final RestClient restClient;
 
+    @CircuitBreaker(name = "bankClient", fallbackMethod = "fallbackDailyRates")
     public List<Valute> getDailyRates() {
         return Objects.requireNonNull(restClient
                         .get()
@@ -22,5 +27,10 @@ public class BankClient {
                         .toEntity(ValCurs.class)
                         .getBody())
                 .getValutes();
+    }
+
+    public List<Valute> fallbackDailyRates(Throwable throwable) {
+        log.warn("Api is unavailable! Fallback method '{}' triggered!","fallbackDailyRates");
+        return Collections.emptyList();
     }
 }
