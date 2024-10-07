324020b1d15f83497879a543b7aca0196b617f4c
stukenvitalii <stukenvitalii@gmail.com>
2024-10-07 19:15:11 +0300

    add exception throwing when 503 occurs

--- src/main/java/edu/tbank/hw8/client/BankClient.java
+++ src/main/java/edu/tbank/hw8/client/BankClient.java
@@ -5,12 +5,13 @@ import edu.tbank.hw8.entity.Valute;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestClient;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 @Slf4j
 @Component
@@ -20,17 +21,24 @@ public class BankClient {
 
     @CircuitBreaker(name = "bankClient", fallbackMethod = "fallbackDailyRates")
     public List<Valute> getDailyRates() {
-        return Objects.requireNonNull(restClient
-                        .get()
-                        .uri("/XML_daily.asp/")
-                        .retrieve()
-                        .toEntity(ValCurs.class)
-                        .getBody())
-                .getValutes();
+        ValCurs valCursResponse = restClient
+                .get()
+                .uri("/XML_daily.asp/")
+                .retrieve()
+                .onStatus(HttpStatusCode::is5xxServerError, (response, request) -> {
+                    log.error("Error response: {}", response);
+                    throw new HttpServerErrorException(HttpStatusCode.valueOf(503));
+                })
+                .toEntity(ValCurs.class)
+                .getBody();
+        if (valCursResponse == null) {
+            throw new HttpServerErrorException(HttpStatusCode.valueOf(503));
+        }
+        return valCursResponse.getValutes();
     }
 
     public List<Valute> fallbackDailyRates(Throwable throwable) {
-        log.warn("Api is unavailable! Fallback method '{}' triggered!","fallbackDailyRates");
+        log.warn("Api is unavailable! Fallback method fallbackDailyRates triggered due to {} ", throwable.getMessage());
         return Collections.emptyList();
     }
 }
