121e90e88712050f0794e878d1cbc029edd3b0f1
stukenvitalii <stukenvitalii@gmail.com>
2024-10-11 20:02:48 +0300

    add scheduler-period property

--- src/main/java/edu/tbank/hw5/configuration/ApplicationConfig.java
+++ src/main/java/edu/tbank/hw5/configuration/ApplicationConfig.java
@@ -3,10 +3,13 @@ package edu.tbank.hw5.configuration;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.time.Duration;
+
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
         @NotNull String kudaGoUrl,
         @NotNull String categoriesEndpoint,
         @NotNull String locationsEndpoint,
-        @NotNull String threads
+        @NotNull Integer threads,
+        @NotNull Duration schedulerPeriod
 ) {}
