41c7db8d4df8b7a369c0e6e48691caa73a879465
stukenvitalii <stukenvitalii@gmail.com>
2024-10-17 11:49:48 +0300

    use Mono and Flux

--- src/main/java/edu/tbank/hw5/service/EventService.java
+++ src/main/java/edu/tbank/hw5/service/EventService.java
@@ -1,12 +1,13 @@
 package edu.tbank.hw5.service;
 
 import edu.tbank.hw5.dto.EventDto;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 public interface EventService {
-    List<EventDto> getEventsByBudget();
-    CompletableFuture<List<EventDto>> getEventsByBudgetAsync(double budget, String currency, LocalDate dateFrom, LocalDate dateTo);
+    Flux<EventDto> getEventsByBudget();
+    Mono<List<EventDto>> getEventsByBudgetAsync(double budget, String currency, LocalDate dateFrom, LocalDate dateTo);
 }
