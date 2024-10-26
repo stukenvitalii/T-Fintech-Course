b40657bd30091c468b7390f60c2107a32baaaf82
stukenvitalii <stukenvitalii@gmail.com>
2024-10-27 00:25:39 +0300

    update LocationController

--- src/main/java/edu/tbank/hw5/controller/LocationController.java
+++ src/main/java/edu/tbank/hw5/controller/LocationController.java
@@ -1,8 +1,10 @@
 package edu.tbank.hw5.controller;
 
 import edu.tbank.hw5.dto.Location;
+import edu.tbank.hw5.memento.LocationMemento;
 import edu.tbank.hw5.service.LocationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.tinkoff.measurementloggingstarter.annotation.Timed;
 
@@ -38,5 +40,11 @@ public class LocationController {
     @DeleteMapping("/delete/{id}")
     public void deleteLocationById(@PathVariable String id) {
         locationService.deleteLocationById(id);
+    }
+
+    @GetMapping("/{id}/history")
+    public ResponseEntity<List<LocationMemento>> getLocationHistoryBySlug(@PathVariable String id) {
+        List<LocationMemento> locationMementos = locationService.getLocationHistoryBySlug(id);
+        return ResponseEntity.ok(locationMementos);
     }
 }
