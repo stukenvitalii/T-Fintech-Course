c683c4c6f673a1a0858c0b99304fd3ce8af86cc8
stukenvitalii <stukenvitalii@gmail.com>
2024-10-28 01:37:10 +0300

    delete unused method

--- src/main/java/edu/tbank/hw5/service/EventService.java
+++ src/main/java/edu/tbank/hw5/service/EventService.java
@@ -2,12 +2,9 @@ package edu.tbank.hw5.service;
 
 import edu.tbank.hw5.dto.EventDto;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.time.LocalDate;
-import java.util.List;
 
 public interface EventService {
-    Flux<EventDto> getEventsByBudget();
-    Mono<List<EventDto>> getEventsByBudgetAsync(Double budget, String currency, LocalDate dateFrom, LocalDate dateTo);
+    Flux<EventDto> getEventsByBudgetAsync(Double budget, String currency, LocalDate dateFrom, LocalDate dateTo);
 }
