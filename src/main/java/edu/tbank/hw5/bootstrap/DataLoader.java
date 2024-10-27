49a02a59b1110ab89c9c1f842d1b223c8f2911a0
stukenvitalii <stukenvitalii@gmail.com>
2024-10-27 14:41:25 +0300

    use Commands

--- src/main/java/edu/tbank/hw5/bootstrap/DataLoader.java
+++ src/main/java/edu/tbank/hw5/bootstrap/DataLoader.java
@@ -1,55 +1,25 @@
 package edu.tbank.hw5.bootstrap;
 
-import edu.tbank.hw5.client.KudaGoClient;
-import edu.tbank.hw5.dto.Category;
-import edu.tbank.hw5.dto.Location;
-import edu.tbank.hw5.observer.DataObserver;
+import edu.tbank.hw5.bootstrap.command.Command;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.tinkoff.measurementloggingstarter.annotation.Timed;
-
-import java.util.List;
 
 @Timed
 @RequiredArgsConstructor
 @Component
 @Slf4j
-public class DataLoader implements CommandLineRunner {
-    private final KudaGoClient kudaGoClient;
-    private final List<DataObserver<Category>> categoryObservers;
-    private final List<DataObserver<Location>> locationObservers;
+public class DataLoader {
+    private final Command retrieveCategoriesCommand;
+    private final Command retrieveLocationsCommand;
 
-    @Override
-    public void run(String... args) {
+    @PostConstruct
+    public void initData() {
         log.info("Starting data loading process");
-        retrieveCategories();
-        retrieveLocations();
+        retrieveCategoriesCommand.execute();
+        retrieveLocationsCommand.execute();
         log.info("Finished data loading process");
-    }
-
-    private void retrieveCategories() {
-        List<Category> categories = kudaGoClient.getAllCategories();
-        if (categories != null) {
-            log.info("Fetched {} categories from API", categories.size());
-            notifyObservers(categoryObservers, categories);
-        } else {
-            log.warn("No categories fetched from API");
-        }
-    }
-
-    private void retrieveLocations() {
-        List<Location> locations = kudaGoClient.getAllLocations();
-        if (locations != null) {
-            log.info("Fetched {} locations from API", locations.size());
-            notifyObservers(locationObservers, locations);
-        } else {
-            log.warn("No locations fetched from API");
-        }
-    }
-
-    private <T> void notifyObservers(List<DataObserver<T>> observers, List<T> data) {
-        observers.forEach(observer -> observer.update(data));
     }
 }
