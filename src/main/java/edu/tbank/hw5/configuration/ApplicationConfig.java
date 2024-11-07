43d7fb97623a28b22ab110fdae3e93ad9ed2d182
stukenvitalii <stukenvitalii@gmail.com>
2024-11-07 22:44:14 +0200

    update application config

--- src/main/java/edu/tbank/hw5/configuration/ApplicationConfig.java
+++ src/main/java/edu/tbank/hw5/configuration/ApplicationConfig.java
@@ -3,9 +3,10 @@ package edu.tbank.hw5.configuration;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-@ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "app")
 public record ApplicationConfig(
         @NotNull String kudaGoUrl,
         @NotNull String categoriesEndpoint,
-        @NotNull String locationsEndpoint
+        @NotNull String locationsEndpoint,
+        @NotNull String secret
 ) {}
