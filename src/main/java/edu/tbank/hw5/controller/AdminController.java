94ed557fccc4a0a8132b76052b270870d8edc868
stukenvitalii <stukenvitalii@gmail.com>
2024-11-07 22:48:44 +0200

    add AdminController

---
+++ src/main/java/edu/tbank/hw5/controller/AdminController.java
@@ -0,0 +1,15 @@
+package edu.tbank.hw5.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Secured data";
+    }
+}
