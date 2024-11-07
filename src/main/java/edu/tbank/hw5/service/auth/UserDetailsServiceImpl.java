268a10bc95e30551b701465531ae59466caa85b9
stukenvitalii <stukenvitalii@gmail.com>
2024-11-07 22:46:32 +0200

    add UserDetailsServiceImpl

---
+++ src/main/java/edu/tbank/hw5/service/auth/UserDetailsServiceImpl.java
@@ -0,0 +1,27 @@
+package edu.tbank.hw5.service.auth;
+
+import edu.tbank.hw5.entity.User;
+import edu.tbank.hw5.repository.jpa.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole().getAuthority())
+                .build();
+    }
+}
