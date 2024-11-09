7f3ede26997c42f2009107eff01db547b8afb92d
stukenvitalii <stukenvitalii@gmail.com>
2024-11-09 20:56:17 +0200

    add JPA reps

--- src/main/java/edu/tbank/hw5/repository/jpa/EventRepositoryJpa.java
+++ src/main/java/edu/tbank/hw5/repository/jpa/EventRepositoryJpa.java
@@ -1,7 +1,30 @@
 package edu.tbank.hw5.repository.jpa;
 
 import edu.tbank.hw5.entity.Event;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public interface EventRepositoryJpa extends JpaRepository<Event, Long> {
+    List<Event> findAll(Specification<Event> specification);
+
+    static Specification<Event> buildSpecification(String title, String place, LocalDate dateFrom, LocalDate toDate) {
+        List<Specification<Event>> specifications = new ArrayList<>();
+        if (title != null) {
+            specifications.add((Specification<Event>) (event, query, cb) -> cb.equal(event.get("title"), title));
+        }
+        if (place != null) {
+            specifications.add((Specification<Event>) (event, query, cb) -> cb.equal(event.get("location").get("name"), place));
+        }
+        if (dateFrom != null && toDate != null) {
+            specifications.add((Specification<Event>)
+                    (event, query, cb) -> cb.between(event.get("date"),
+                            Timestamp.valueOf(dateFrom.atStartOfDay()), Timestamp.valueOf(toDate.atStartOfDay())));
+        }
+        return specifications.stream().reduce(Specification::and).orElse(null);
+    }
 }
--- src/main/java/edu/tbank/hw5/repository/jpa/LocationRepositoryJpa.java
+++ src/main/java/edu/tbank/hw5/repository/jpa/LocationRepositoryJpa.java
@@ -2,6 +2,11 @@ package edu.tbank.hw5.repository.jpa;
 
 import edu.tbank.hw5.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface LocationRepositoryJpa extends JpaRepository<Location, Long> {
+    @Query("SELECT l FROM Location l JOIN FETCH l.events WHERE l.id = :id")
+    Optional<Location> findLocationById(Long id);
 }
